64cfadedf8f934f450586039568538149de632b1
Mel <constantine.dudorov@yandex.ru>
2024-02-05 13:16:23 +0300

    Сделана Регистрация

--- static/files/logreg-script.js
+++ static/files/logreg-script.js
@@ -32,6 +32,43 @@ signUp.addEventListener("click", () => {
 login.addEventListener("click", () => {
     container.classList.remove("active");
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const registerButton = document.querySelector('.form.signup input[type="button"]');
+
+    registerButton.addEventListener('click', function() {
+        const firstName = document.querySelector('.form.signup input[placeholder="Имя"]').value;
+        const lastName = document.querySelector('.form.signup input[placeholder="Фамилия"]').value;
+        const phoneNumber = document.querySelector('.form.signup input[placeholder="Телефон"]').value;
+        const email = document.querySelector('.form.signup input[placeholder="Электронная почта"]').value;
+        const password = document.querySelector('.form.signup input[placeholder="Пароль"]').value;
+
+        const userData = {
+            first_name: firstName,
+            last_name: lastName,
+            number: phoneNumber,
+            email: email,
+            password: password
+        };
+
+        // Отправка данных на сервер
+        fetch('http://localhost:8080/api/v1/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Ошибка при сохранении пользователя');
+                }
+                console.log('Пользователь успешно сохранен');
+            })
+            .catch(error => {
+                console.error('Произошла ошибка:', error);
+            });
+    });
+});
 
 // jQuery code to appear and disappear fields
 $(document).ready(function () {
